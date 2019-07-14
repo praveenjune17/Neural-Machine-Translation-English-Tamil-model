6eae785296fe545d403b72c017ce7d3483c25947
Praveenkumar <praveen1050208@gmail.com>
2019-07-14 11:19:11 +0530

    Add files via upload

--- en_tam_parallel_text.py
+++ en_tam_parallel_text.py
@@ -7,67 +7,63 @@ from __future__ import print_function
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import os
+import tempfile
+import shutil
 
 
-_CITATION = """\
-                                       Dataset 1
-@inproceedings {
-    biblio:RaBoMorphologicalProcessing2012,
+_CITATION = """\  
+@inproceedings {biblio:RaBoMorphologicalProcessing2012,
 	title = {Morphological Processing for English-Tamil Statistical Machine Translation},
 	author = {Loganathan Ramasamy and Ond{\v{r}}ej Bojar and Zden{\v{e}}k {\v{Z}}abokrtsk{\'{y}}},
 	year = {2012},
 	pages = {113--122},
 	Booktitle = {Proceedings of the Workshop on Machine Translation and Parsing in Indian Languages ({MTPIL}-2012)},
 }
-                                       Dataset 2
-                                       (OPUS:- collection of many OpenSubtitles corpus)									   
-Corpus Name: Tanzil 
-     Package: Tanzil in Moses format
-     Website: http://opus.nlpl.eu/Tanzil-v1.php
-Corpus Name: Ubuntu
-     Package: Ubuntu.en-ta_LK in Moses format
-     Website: http://opus.nlpl.eu/Ubuntu-v14.10.php
-Corpus Name: GNOME
-     Package: GNOME.en-ta in Moses format
-     Website: http://opus.nlpl.eu/GNOME-v1.php
-Corpus Name: KDE4
-     Package: KDE4.en-ta in Moses format
-     Website: http://opus.nlpl.eu/KDE4-v2.php
-Corpus Name: OpenSubtitles
-     Package: OpenSubtitles in Moses format
-     Website: http://opus.nlpl.eu/OpenSubtitles-v2018.php
-	 
-                                        Dataset 3
-										https://github.com/joshua-decoder/indian-parallel-corpora/blob/master/LICENSE
+
+@InProceedings{TIEDEMANN12.463,
+  author = {J�rg Tiedemann},
+  title = {Parallel Data, Tools and Interfaces in OPUS},
+  booktitle = {Proceedings of the Eight International Conference on Language Resources and Evaluation (LREC'12)},
+  year = {2012},
+  month = {may},
+  date = {23-25},
+  address = {Istanbul, Turkey},
+  editor = {Nicoletta Calzolari (Conference Chair) and Khalid Choukri and Thierry Declerck and Mehmet Ugur Dogan and Bente Maegaard and Joseph Mariani and Jan Odijk and Stelios Piperidis},
+  publisher = {European Language Resources Association (ELRA)},
+  isbn = {978-2-9517408-7-7},
+  language = {english}
+ }	 
+                                        
+https://github.com/joshua-decoder/indian-parallel-corpora/blob/master/LICENSE
 										
 """
 _DESCRIPTION = """\
-This corpus is a collection of english and tamil parallel text extracted 3 sources (mentioned above as dataset 1, dataset 2, dataset 3).
+This corpus is a collection of english and tamil parallel text extracted 3 sources.
 All the three were merged into a single file and cleaned.The various data cleaning steps were
 a)Removed duplicates lines
 b)Removed lines containing html tags
 c) Removed empty lines and lines which contains an empty english or tamil translation
-d) Removed short english transciptions with word length len(word) <= 2
-After cleaning there are total of 380245 lines in the corpus.All the lines which were removed were collected in a separate text file
+d) Manually inspected and removed junk characters
+There are total of 370,000 (roughly) lines of text which were split to train, validation and test sets. 
 Script used for data cleaning :- https://colab.research.google.com/drive/1AViiS_4-ClngwZ4bKYbmlq3cVAnNHCk0#scrollTo=MWn-AuohbfXI
+
 Note:-
-For dataset-3, the data was created by non-expert translators hired over Mechanical Turk and other 
-datasets were created from extracting text from movie subtitles, Bible, Quran, cinema, news so the quality of translations are of mixed.
+For dataset-3, the data was created by non-expert translators hired over Mechanical Turk and the other 
+datasets were created from extracting text from movie subtitles, Bible, Quran, cinema and news websites so the quality of translations are of mixed.
 However, it should be useful enough to get you started training models.
+
 Reference for dataset 1 :- http://ufal.mff.cuni.cz/~ramasamy/parallel/html/
 Reference for dataset 2 :- http://opus.nlpl.eu/
 Reference for dataset 3 :- https://github.com/joshua-decoder/indian-parallel-corpora
 """
-test_file_link    = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_test.tar.gz'
-valid_file_link    = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_valid.tar.gz'
-train_file_1_link = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_train_split1.tar.gz'
-train_file_2_link = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_train_split2.tar.gz'
-train_file_3_link = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_train_split3.tar.gz'
+test_file_link = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_test.tar.gz'
+valid_file_link = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_valid.tar.gz'
+train_file_link = 'https://drive.google.com/uc?export=download&id=1eet9TaAKoEXEU1gU4xGQxvhl6oJWAZbA'
+#train_file_2_link = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_train_split2.tar.gz'
+#train_file_3_link = 'https://github.com/praveenjune17/Neural-Machine-Translation-English-Tamil-model/raw/master/en_tam_parallel_text_train_split3.tar.gz'
 
 class EnTamParallelText(tfds.core.GeneratorBasedBuilder):
-  """TODO(en_tam_parallel_text): Short description of my dataset."""
-
-  # TODO(en_tam_parallel_text): Set up version.
+  """(en_tam_parallel_text): English_Tamil parallel text corpus"""
   VERSION = tfds.core.Version('0.1.0')
   def _info(self):
     return tfds.core.DatasetInfo(
@@ -75,36 +71,43 @@ class EnTamParallelText(tfds.core.GeneratorBasedBuilder):
         supervised_keys=("input", "target"), citation=_CITATION,)
 
   def _split_generators(self, dl_manager):
-    """Load the data from the manual_dir"""
-    # dl_manager is a tfds.download.DownloadManager that can be used to
-    # download and extract URLs
-	# download link ='https://drive.google.com/uc?export=download&confirm=WCqK&id=12kSyzoUZo8k4BYoU-LzqkfS0-rfw-i4U'
-	# downloaded file_name =en_tam_parrallel_text_nodups_sorted_eng_tam_reversed.zip
-	#copy path :-  usr/local/lib/python3.6/dist-packages/tensorflow_datasets/translate/en_tam_parallel_text.py
-	#checksum path :- usr/local/lib/python3.6/dist-packages/tensorflow_datasets/url_checksums
-	# Return the data as 3 splits.
+    """Load the data from the github links provided above"""
     extracted_path_test = dl_manager.download_and_extract(test_file_link)
     extracted_path_valid = dl_manager.download_and_extract(valid_file_link)
-    extracted_path_train = dl_manager.download([train_file_1_link,train_file_2_link,train_file_3_link])
-    print(extracted_path_train)
-    os.system('''cat /root/tensorflow_datasets/downloads/prav_Neur-Mach-Tran-Engl-Tami-mode_raw_a7-jw1afIzryZDxHLxn886ieInxXmgaP9rhhjeZwesI.tar.gz /root/tensorflow_datasets/downloads/prav_Neur-Mach-Tran-Engl-Tami-mode_raw___HgTPv00ZHCCeRb7VVkwQOvGJtMWRm8cSM9C2KYCP4.tar.gz /root/tensorflow_datasets/downloads/prav_Neur-Mach-Tran-Engl-Tami-mode_raw_7UHyQWdhPnGAORqS9kyetxl7rhl2a4n99boGVX65mfA.tar.gz > /root/tensorflow_datasets/downloads/en_tam_parallel_corpus_train.tar.gz''')
-    #print(os.path.exists("../root/tensorflow_datasets/downloads/en_tam_parallel_corpus_train.tar.gz"),'********************************')
-    extracted_path_train = dl_manager.extract("../root/tensorflow_datasets/downloads/en_tam_parallel_corpus_train.tar.gz")
+    extracted_path_train = dl_manager.download_and_extract(train_file_link)
+    #extracted_path_train = dl_manager.extract([next(dl_manager.iter_archive(dl_manager.download(train_file_1_link)))[1], 
+	#                            next(dl_manager.iter_archive(dl_manager.download(train_file_2_link)))[1],
+	#                            next(dl_manager.iter_archive(dl_manager.download(train_file_3_link)))[1]])   
+	#temp2 = dl_manager.iter_archive(train_file_2_link)
+	#temp3 = dl_manager.iter_archive(train_file_3_link)
+	#op_path = downloaded_path_train+
+    #print('*********************')
+    #print(tf.io.gfile.Glob(os.path.join(downloaded_path_train,'*.tar.gz')))
+    #for file in [next(dl_manager.iter_archive(dl_manager.download(train_file_1_link)))[0],
+    #             next(dl_manager.iter_archive(dl_manager.download(train_file_2_link)))[0],
+    #             next(dl_manager.iter_archive(dl_manager.download(train_file_3_link)))[0]]:
+    #  with (tf.io.gfile.GFile(file,'rb')) as fp:
+    #    temp += fp.read()
+    #    extracted_path_train = dl_manager.extract(temp)
+    #  for filename in tf.io.gfile.Glob(os.path.join(downloaded_path_train,'*.tar.gz')):
+    #    with open(filename,'rb') as readfile:
+    #      shutil.copyfileobj(readfile, fp)
+    #  extracted_path_train=dl_manager.extract(fp)
+	  #tf.gfile.ListDirectory(extracted_path_train)
+    #os.system('''cat /root/tensorflow_datasets/downloads/prav_Neur-Mach-Tran-Engl-Tami-mode_raw_a7-jw1afIzryZDxHLxn886ieInxXmgaP9rhhjeZwesI.tar.gz /root/tensorflow_datasets/downloads/prav_Neur-Mach-Tran-Engl-Tami-mode_raw___HgTPv00ZHCCeRb7VVkwQOvGJtMWRm8cSM9C2KYCP4.tar.gz /root/tensorflow_datasets/downloads/prav_Neur-Mach-Tran-Engl-Tami-mode_raw_7UHyQWdhPnGAORqS9kyetxl7rhl2a4n99boGVX65mfA.tar.gz > /root/tensorflow_datasets/downloads/en_tam_parallel_corpus_train.tar.gz''')
+    #extracted_path_train = dl_manager.extract("../root/tensorflow_datasets/downloads/en_tam_parallel_corpus_train.tar.gz")
     return [
-          tfds.core.SplitGenerator(name=tfds.Split.TRAIN, num_shards=6,\
+          tfds.core.SplitGenerator(name=tfds.Split.TRAIN, num_shards=2,\
               gen_kwargs={"data_file":os.path.join(extracted_path_train,'en_tam_parallel_corpus_train')}),
-          tfds.core.SplitGenerator(name=tfds.Split.VALIDATION, num_shards=4,\
+          tfds.core.SplitGenerator(name=tfds.Split.VALIDATION, num_shards=1,\
               gen_kwargs={"data_file":os.path.join(extracted_path_valid,'en_tam_parallel_corpus_validation')}),
-        #os.path.join(extracted_path, "en_tam_parallel_corpus_validation")}),\
-          tfds.core.SplitGenerator(name=tfds.Split.TEST, num_shards=4,\
+          tfds.core.SplitGenerator(name=tfds.Split.TEST, num_shards=1,\
               gen_kwargs={"data_file":os.path.join(extracted_path_test,'en_tam_parallel_corpus_test')}),
-        #os.path.join(extracted_path_test, "en_tam_parallel_corpus_test")})
       ]
 
   def _generate_examples(self, data_file):
     with tf.io.gfile.GFile(data_file) as dataset:
       for line in dataset:
         ip, target = line.strip().split('\t')
-        #ip, target = line_parts[0].strip(), line_parts[1].strip()
         yield {"input": ip, "target": target}
 		
--- en_tam_parallel_text_test.py
+++ en_tam_parallel_text_test.py
@@ -10,14 +10,13 @@ from tensorflow_datasets.translate import en_tam_parallel_text
 #Overlapping lines in test , train       (3 lines overlapping)
 #Overlapping lines in test , validation  (1 line overlapping)
 #Not able to remove the overlapping lines since only the hashvalue of the lines are thrown out in the Traceback  but not the actual line or its index 
+#Acutal count of lines of text {"test" : 26000, "train" : 324196, "validation" : 25245}
 
 class EnTamParallelTextTest(testing.DatasetBuilderTestCase):
   DATASET_CLASS = en_tam_parallel_text.EnTamParallelText
-  OVERLAPPING_SPLITS = ['test', 'validation']
-  SPLITS = {"test"  : 26000 ,
-            "train" : 324196 , 
-			"validation" : 25245}
-  #DL_EXTRACT_RESULT = {'data_file' : 'en_tam_parallel_corpus_test'}
+  #OVERLAPPING_SPLITS = ['test', 'validation']
+  SPLITS = {"test" : 2, "train" : 2, "validation" : 2}
+
 
 
 if __name__ == "__main__":
